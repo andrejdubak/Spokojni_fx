b98c606a5c81748f9de3ba5c375e36ee23d6dbda
Rastislav Kopca <rastislavkopca@gmail.com>
2022-04-08 15:08:26 +0200

    admin screen

---
+++ src/main/java/com/example/spokojni/frontend/AdminPopupController.java
@@ -0,0 +1,21 @@
+package com.example.spokojni.frontend;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class AdminPopupController {
+
+    @FXML
+    private Button Login;
+
+    @FXML
+    private Button deleteUser;
+
+    @FXML
+    private Text name;
+
+    @FXML
+    private Button newPassword;
+
+}
--- src/main/java/com/example/spokojni/frontend/AdminViewController.java
+++ src/main/java/com/example/spokojni/frontend/AdminViewController.java
@@ -1,4 +1,44 @@
 package com.example.spokojni.frontend;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+
 public class AdminViewController {
+
+    @FXML
+    private Button Profile;
+
+    @FXML
+    private Button Students;
+
+    @FXML
+    private Button Teachers;
+
+    @FXML
+    private Button exportPeople;
+
+    @FXML
+    private Button importPeople;
+
+    @FXML
+    private Button logOut;
+
+    @FXML
+    private Text name;
+
+    @FXML
+    private Button registerPerson;
+
+    @FXML
+    private TextField search;
+
+
+    @FXML
+    private void logoutClick() throws IOException{
+        new ChangeWindowController(logOut, "login-view.fxml");
+    }
 }
